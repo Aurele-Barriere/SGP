ff841c157dbd8c95a575dd0d7ce9e6c2221dc3b6
Jérémy Thibault <jeremy.thibault@ens-rennes.fr>
2016-09-22 21:36:57 +0200

    added a generic function to check for error and abort

--- TP1/partie1.c
+++ TP1/partie1.c
@@ -4,28 +4,33 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+
+// Generic function to check if an error occured,
+// and exits then.
+void check_perror(int n) {
+  if (n == -1) {
+    perror("Error: ");
+    exit(EXIT_FAILURE);
+  }
+}
+
 int main() {
 
   pid_t pid_son;
   int son_status;
 
   // Forking
-
   pid_son = fork();
-  if (pid_son == -1) {
-    perror("Error: ");
-    exit(EXIT_FAILURE);
-  }
+  check_perror(pid_son);
+
   if (pid_son == 0) {
-    // This is the son process, we replace our code
-    // with partie2's code
-    if (execl("partie2", "partie2", (char*) NULL) == -1) {
-      perror("Error: ");
-      exit(EXIT_FAILURE);
-    }
+    // This is the son process
+    // Replaces the executed code. Iff there's an error the return value will be checked.
+    check_perror(execl("partie2", "partie2", (char*) NULL));
   } else {
     // If we are in the father process, we wait
     // for the son to end.
+
     if (pid_son == wait(&son_status)) {
       // The son process that ended was the one we created.
       // This should not be a problem but we must be sure
@@ -35,8 +40,7 @@ int main() {
       }
     } else {
       // The only possible case is that pid_son == -1, there was an error.
-      perror("Error: ");
-      exit(EXIT_FAILURE);
+      check_perror(pid_son);
     }
   }
 
