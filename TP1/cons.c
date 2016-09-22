6a7a08fabec8fa961f952776def26e208a9bb571
Jérémy Thibault <jeremy.thibault@ens-rennes.fr>
2016-09-22 23:01:00 +0200

    Third exercise completed. Cleaned and commented a bit.

--- TP1/cons.c
+++ TP1/cons.c
@@ -7,33 +7,47 @@
 #include <string.h>
 #include <fcntl.h>
 
-
 #define BUFFER_SIZE 1024
 
-
+// Generic function to check if an error occured,
+// and exits then.
+// See prod.c for details.
+void check_perror_pipe(int n, int* fd) {
+  if (n == -1) {
+    perror("Error: ");
+    if (fd) {
+      close(*fd);
+    }
+    exit(EXIT_FAILURE);
+  }
+}
 
 
 void consummer(int fd) {
-  
-  int read_chars;
+
+  int read_chars; // number of read chars.
   char buffer[BUFFER_SIZE];
-  
-  while (read_chars = read(fd, buffer, 1)) {
+
+  // Main loop of the consummer process.
+  // Reads characters one by one from the pipe, and display them vertically, on the side.
+  do {
+    read_chars = read(fd, buffer, 1);
+    check_perror_pipe(read_chars, &fd);
+
     if (buffer[0] != '\n') {
+      // This condition ensures we do not write an extra '\n'
       printf("                                    <consummer> %c\n", buffer[0]);
     }
-    
-  }
-  close (fd);
-  exit(1);
+  } while (read_chars);
+
+  close(fd);
 }
 
 int main() {
   int fd_pipe;
   fd_pipe = open("pipe", O_RDONLY);
-  if (fd_pipe == -1) {
-    exit(1);
-  }
+  check_perror_pipe(fd_pipe, NULL);
+
   consummer(fd_pipe);
   return 0;
 }
--- TP1/prod.c
+++ TP1/prod.c
@@ -7,59 +7,60 @@
 #include <string.h>
 #include <fcntl.h>
 
-
 #define BUFFER_SIZE 1024
 
+// Generic function to check if an error occured,
+// and exits then.
+// Also closes the given pipe file descriptor if its not NULL.
+//
+// We use a pointer to emulate an option type: if fd == NULL, it means there is no
+// file to close.
+void check_perror_pipe(int n, int* fd) {
+  if (n == -1) {
+    perror("Error: ");
+    if (fd) {
+      close(*fd);
+    }
+    exit(EXIT_FAILURE);
+  }
+}
 
 void producer(int fd) {
-  
+
   char buffer[BUFFER_SIZE];
-  buffer[0] = '\0';
-  
-  while (buffer[0] != '\n') {
+  int written_chars; // number of written chars
+
+  // Main producer loop: tries to read entry from the standard input
+  // until the user submits an "empty" chain (ie a single '\n' character)
+  do {
+
     printf("<producer> Type something in :\n");
     if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
       // Problem during input
-      exit (1);
+      close(fd);
+      exit(EXIT_FAILURE);
     }
-    write(fd, buffer, strlen(buffer));
-  }
-  
-  close (fd);  
+
+    written_chars = write(fd, buffer, strlen(buffer));
+    check_perror_pipe(written_chars, &fd);
+  } while (buffer[0] != '\n');
+
+  close(fd);
 }
 
 
 int main() {
-  
-  pid_t pid_cons;
+
   int fd_pipe;
-  
-  // Creating the pipe
-  if (mknod("pipe", S_IFIFO | 0666,0) == -1) {
-    exit(1);
-  }
-  
-  // Forking
-  pid_cons = fork();
 
-  if (pid_cons==-1) {
-    return 1; // Failed to fork
-  }
-  
-  if (pid_cons==0) {
-    // If we are the son 
-    execl("cons", NULL);
-  }
+  // Creates the pipe
+  check_perror_pipe(mknod("pipe", S_IFIFO | 0666,0), NULL);
 
-  else {
-    // If we are the father
-    if ((fd_pipe = open("pipe", O_WRONLY)) == -1) {
-      exit(1);
-    }
-    
-    producer(fd_pipe);
-  }
+  // Producer process
+  fd_pipe = open("pipe", O_WRONLY);
+  check_perror_pipe(fd_pipe, NULL);
 
- 
+  producer(fd_pipe);
+
   return 0;
 }
