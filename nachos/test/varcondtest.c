5d288b918ae4c96a2d86c6e44991f8bdd74a82c3
Jérémy Thibault <jeremy.thibault@ens-rennes.fr>
2016-11-17 17:01:09 +0100

    explained the varcondtest program

--- nachos/test/varcondtest.c
+++ nachos/test/varcondtest.c
@@ -5,8 +5,18 @@
 SemId sema;
 CondId cond;
 
+/*
+  This program aims to test the behaviour of condition variables.
+  However, the program shouldn't be considered as a good example outside of this particular OS.
+  Indeed, in general there's no way to be sure that some thread is waiting on a condition variable. If it sends a signal (or a V, or modify a variable) to warn that it's going to wait, it could always been interrupted between the signal and the wait. Disabling interrupts to solve this would not be a good solution either as it would be a wait in a critical section and a possible source of deadlock.
+  Luckily, in our OS, the scheduler will never interrupt our threads on something differerent from a P or a Wait. Thus, we can use a V on a semaphore to warn the thread that we're going to wait without being interrupted, and then the other thread can wake the other threads with Signal or Broadcast.
 
-void doublewait () {
+  In brief, this program works in our specific case, but don't test it on another OS.
+ */
+
+
+
+void doubleWait () {
   // This first thread wait for the broadcast, then for the signal on the condition variable
   V(sema);
   CondWait(cond);
@@ -16,14 +26,14 @@ void doublewait () {
   n_printf("Signal received for first thread\n");
 }
 
-void simplewait () {
+void simpleWait () {
   // The second threads only waits for the broadcast
   V(sema);
   CondWait(cond);
   n_printf("Broadcast received for second thread\n");
 }
 
-void signalbroadcast () {
+void signalBroadcast () {
   // Once the previous threads are waiting, broadcast on the condition variable
   P(sema);
   P(sema);
@@ -41,9 +51,9 @@ void signalbroadcast () {
 int main() {
   sema = SemCreate("sema",0);
   cond = CondCreate("condition variable");
-  ThreadId t1 = threadCreate("t1", doublewait);
-  ThreadId t2 = threadCreate("t2", simplewait);
-  ThreadId t3 = threadCreate("t3", signalbroadcast);
+  ThreadId t1 = threadCreate("t1", doubleWait);
+  ThreadId t2 = threadCreate("t2", simpleWait);
+  ThreadId t3 = threadCreate("t3", signalBroadcast);
 
   Join(t1);
   Join(t2);
